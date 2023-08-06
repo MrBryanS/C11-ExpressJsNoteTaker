ee53cbcc5a1b9439679d8d21522c0e8acd8e7c80
Bryan Stauning <bryanstauning@gmail.com>
2023-08-06 16:44:34 -0500

    adding code for tech support

---
+++ Develop/server.js
@@ -0,0 +1,24 @@
+
+// Dependencies
+const express = require(`express`);
+const app = express();
+const PORT = 3001;
+
+// read the json file and store it in a variable
+const termData = require('./db/db.json');
+// print the json file to the console
+console.log(termData);
+
+
+
+app.get('/', (req, res) => {
+     res.send(
+       return termData
+   });
+  
+  // activate the server
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
+
+
