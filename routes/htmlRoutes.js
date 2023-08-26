aa0c80bd78d1ecdc024834e0d42b9078fb40a86c
Bryan Stauning <bryanstauning@gmail.com>
2023-08-26 12:26:08 -0500

    change * to / for heroku routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -9,13 +9,11 @@ router.get('/notes', (req, res) => {
 
 
 //20230826-1025 change '*' to `/` if solves heroku issue: method=GET path="/" 
-
   // router.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname, '../public/index.html'));
 //   });
 
-
-router.get('*', (req, res) => {
+router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 
